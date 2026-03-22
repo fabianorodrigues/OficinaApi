609de6b353332eebd5b1635921a8902c9aac1e84
jaqueline ramos da silva <jaquelineramosit@hotmail.com>
2026-03-22 20:38:46 -0300

    inclusao de accesse token no workflow

--- src/terraform/kubernets/main.tf
+++ src/terraform/kubernets/main.tf
@@ -1,9 +1,67 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "" # Vai usar KUBECONFIG do ambiente (GitHub Actions)
 }
 
+# Namespace
 resource "kubernetes_namespace" "oficina" {
   metadata {
     name = var.namespace_name
+  }
+}
+
+# Deployment
+resource "kubernetes_deployment" "oficina_app" {
+  metadata {
+    name      = "oficina-app"
+    namespace = kubernetes_namespace.oficina.metadata[0].name
+  }
+
+  spec {
+    replicas = var.replicas
+
+    selector {
+      match_labels = {
+        app = "oficina-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "oficina-app"
+        }
+      }
+
+      spec {
+        container {
+          name  = "oficina-api"
+          image = "${var.docker_image_repo}:${var.docker_image_tag}" # tag dinâmica
+          port {
+            container_port = var.container_port
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service LoadBalancer
+resource "kubernetes_service" "oficina_service" {
+  metadata {
+    name      = "oficina-service"
+    namespace = kubernetes_namespace.oficina.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.oficina_app.metadata[0].labels["app"]
+    }
+
+    port {
+      port        = var.service_port
+      target_port = var.container_port
+    }
+
+    type = "LoadBalancer"
   }
 }
--- src/terraform/kubernets/variables.tf
+++ src/terraform/kubernets/variables.tf
@@ -1,5 +1,35 @@
 variable "namespace_name" {
-  description = "Nome do namespace no Kubernetes"
+  description = "Nome do namespace do Kubernetes"
   type        = string
   default     = "oficina"
+}
+
+variable "docker_image_repo" {
+  description = "Repositório Docker sem tag"
+  type        = string
+  default     = "jaquelineramosit/oficina-api"
+}
+
+variable "docker_image_tag" {
+  description = "Tag da imagem Docker (ex: latest ou SHA do commit)"
+  type        = string
+  default     = "latest"
+}
+
+variable "replicas" {
+  description = "Número de réplicas do deployment"
+  type        = number
+  default     = 1
+}
+
+variable "container_port" {
+  description = "Porta que o container expõe"
+  type        = number
+  default     = 8080
+}
+
+variable "service_port" {
+  description = "Porta do serviço Kubernetes"
+  type        = number
+  default     = 80
 }
