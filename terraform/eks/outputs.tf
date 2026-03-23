0ae410c5b7357dcd89cf3e26c016580a65e57650
jaqueline ramos da silva <jaquelineramosit@hotmail.com>
2026-03-23 14:30:11 -0300

    corrigi a variavel eks do output

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = module.eks.cluster_id
+  value = module.eks-cluster.cluster_id
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = module.eks-cluster.cluster_endpoint
 }
 
 output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+  value = module.eks-cluster.cluster_certificate_authority_data
 }
 
 output "node_group_names" {
-  value = keys(module.eks.eks_managed_node_groups)
+  value = keys(module.eks-cluster.eks_managed_node_groups)
 }
