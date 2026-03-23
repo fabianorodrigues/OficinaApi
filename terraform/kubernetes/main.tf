75ab70f5b90217e899ad70f783e7c2c9e2f76a9e
jaqueline ramos da silva <jaquelineramosit@hotmail.com>
2026-03-23 19:36:18 -0300

    ajuste na main

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -1,5 +1,6 @@
+# --- PROVIDER E DATAS (Igual ao seu) ---
 data "aws_eks_cluster" "cluster" {
-  name = "EKS-Oficina" # Confirme se esse é o nome exato do seu cluster
+  name = "EKS-Oficina"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
@@ -12,13 +13,37 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-
+# --- 1. NAMESPACE ---
 resource "kubernetes_namespace_v1" "oficina" {
   metadata {
     name = var.namespace_name
   }
 }
 
+# --- 2. CONFIGMAP ---
+resource "kubernetes_config_map_v1" "oficina_config" {
+  metadata {
+    name      = "oficina-config"
+    namespace = kubernetes_namespace_v1.oficina.metadata[0].name
+  }
+  data = {
+    ASPNETCORE_ENVIRONMENT = "Development"
+  }
+}
+
+# --- 3. SECRET ---
+resource "kubernetes_secret_v1" "oficina_secret" {
+  metadata {
+    name      = "oficina-secret"
+    namespace = kubernetes_namespace_v1.oficina.metadata[0].name
+  }
+  type = "Opaque"
+  string_data = {
+    CONNECTION_STRING = "Server=sqlserver,1433;Database=OficinaDb;User Id=sa;Password=Your_password123!;TrustServerCertificate=True;"
+  }
+}
+
+# --- 4. DEPLOYMENT ---
 resource "kubernetes_deployment_v1" "oficina_app" {
   metadata {
     name      = "oficina-app"
@@ -30,7 +55,6 @@ resource "kubernetes_deployment_v1" "oficina_app" {
 
   spec {
     replicas = var.replicas
-
     selector {
       match_labels = {
         app = "oficina-api"
@@ -49,24 +73,24 @@ resource "kubernetes_deployment_v1" "oficina_app" {
           name  = "oficina-api"
           image = "${var.docker_image_repo}:${var.docker_image_tag}"
           
-          # AS VARIÁVEIS DE AMBIENTE FICAM AQUI NO DEPLOYMENT!
+          # Injeta todas as chaves do ConfigMap e da Secret como variáveis de ambiente
           env_from {
             config_map_ref {
-              name = "oficina-config"
+              name = kubernetes_config_map_v1.oficina_config.metadata[0].name
             }
           }
-
           env_from {
             secret_ref {
-              name = "oficina-secret"
+              name = kubernetes_secret_v1.oficina_secret.metadata[0].name
             }
           }
 
+          # Mapeamento para o nome exato que o seu código .NET espera
           env {
             name = "ConnectionStrings__SqlServer" 
             value_from {
               secret_key_ref {
-                name = "oficina-secret"
+                name = kubernetes_secret_v1.oficina_secret.metadata[0].name
                 key  = "CONNECTION_STRING"
               }
             }
@@ -81,6 +105,7 @@ resource "kubernetes_deployment_v1" "oficina_app" {
   }
 }
 
+# --- 5. SERVICE ---
 resource "kubernetes_service_v1" "oficina_service" { 
   metadata {
     name      = "oficina-service"
