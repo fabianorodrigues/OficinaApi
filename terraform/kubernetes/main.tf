2b2eeb2028eea00e54622d1fb80bc9a7cfd5452a
jaqueline ramos da silva <jaquelineramosit@hotmail.com>
2026-03-23 17:29:23 -0300

    alteracao na main para ajustar o erro de localhost

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -1,17 +1,33 @@
-provider "kubernetes" {
-  config_path = "" # Usa KUBECONFIG gerado no workflow
+# 1. Ajuste o Provider para buscar as credenciais do EKS na AWS
+data "aws_eks_cluster" "cluster" {
+  name = "EKS-Oficina" # Confirme se esse é o nome exato do seu cluster
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = "EKS-Oficina"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# 2. Namespace
 resource "kubernetes_namespace" "oficina" {
   metadata {
     name = var.namespace_name
   }
 }
 
+# 3. Deployment (Com as labels alinhadas)
 resource "kubernetes_deployment" "oficina_app" {
   metadata {
     name      = "oficina-app"
     namespace = kubernetes_namespace.oficina.metadata[0].name
+    labels = {
+      app = "oficina-api"
+    }
   }
 
   spec {
@@ -19,14 +35,14 @@ resource "kubernetes_deployment" "oficina_app" {
 
     selector {
       match_labels = {
-        app = "oficina-app"
+        app = "oficina-api"
       }
     }
 
     template {
       metadata {
         labels = {
-          app = "oficina-app"
+          app = "oficina-api"
         }
       }
 
@@ -43,13 +59,11 @@ resource "kubernetes_deployment" "oficina_app" {
   }
 }
 
+# 4. Service (Tipo LoadBalancer)
 resource "kubernetes_service" "oficina_service" {
   metadata {
     name      = "oficina-service"
     namespace = kubernetes_namespace.oficina.metadata[0].name
-    labels = {
-      app = "oficina-api"
-    }
   }
 
   spec {
@@ -64,4 +78,9 @@ resource "kubernetes_service" "oficina_service" {
 
     type = "LoadBalancer"
   }
+}
+
+# 5. Output da URL (Para você ver no Github Actions)
+output "url_da_api" {
+  value = kubernetes_service.oficina_service.status[0].load_balancer[0].ingress[0].hostname
 }
