7968e9c927e899bc3d8611c78b6c924b98fd04ba
jaqueline ramos da silva <jaquelineramosit@hotmail.com>
2026-03-23 13:37:41 -0300

    correcao eks terraform

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -4,28 +4,31 @@ provider "aws" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "~> 20.0" # Versão estável recomendada
 
-  name               = var.cluster_name
-  kubernetes_version = var.cluster_version
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
 
-  # Role existente do cluster EKS
-  cluster_iam_role_name = var.cluster_iam_role_name
+  # Cluster IAM Role - Usando role existente
+  create_iam_role          = false
+  iam_role_arn             = var.cluster_iam_role_arn
 
-  # Node group gerenciado usando role existente
+  # Acesso ao cluster
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     oficina_nodes = {
-      instance_types     = var.node_instance_types
-      min_size           = var.min_capacity
-      max_size           = var.max_capacity
-      desired_size       = var.desired_capacity
-      node_iam_role_name = var.node_iam_role_name
+      instance_types = var.node_instance_types
+      min_size     = var.min_capacity
+      max_size     = var.max_capacity
+      desired_size = var.desired_capacity
+
+      # Node IAM Role - Usando role existente
+      create_iam_role = false
+      iam_role_arn    = var.node_iam_role_arn
     }
   }
-
-  # Concede permissões de admin no kubeconfig automaticamente
-  enable_cluster_creator_admin_permissions = true
 }
